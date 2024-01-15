09451b009bdc4fd7954bc659b4ce7cd5db32c42c
Ilya K <131490186+umphmeow@users.noreply.github.com>
2024-01-15 10:27:35 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
-import math
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
 
-p = [(1, 1), (2, 2), (1, 2)]
-def distancy(p1, p2):
-    x1, y1 = p1
-    x2, y2 = p2
-    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
-def pair(p):
-    min_distance = float('inf')
-    closest = None
-    for i in range(len(p)):
-        for j in range(i + 1, len(p)):
-            dist = distancy(p[i], p[j])
-            if dist < min_distance:
-                min_distance = dist
-                closest = (p[i], p[j])
-    return closest
+result = 0
+for row in matrix:
+    min_elements = [element for element in row if element == min(row)]
+    result += sum(min_elements)
 
-print("Ближайшая пара точек:", pair(p))
+print(f"Сумма минимальных элементов из каждой строки: {result}")
